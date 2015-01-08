381690e50266d413b59feb137e4636981e906971
Alex Satrapa <grail@goldweb.com.au>
2015-01-08 19:33:52 +1100

    Added LeoStick pins

--- pins.h
+++ pins.h
@@ -1,8 +1,8 @@
 #ifndef PINS
 #define PINS
 
+// Arduino Pins
 // DIGITAL I/O
-
 // Pin 0 UART RX, PCINT16
 // Pin 1 UART TX, PCINT17
 // Pin 2 interrupt 0, PCINT18
@@ -21,8 +21,36 @@
 // Pin 13 SPI SCK, LED
 
 // ANALOGUE I/O
-#define I2C_DA 5 // I2C data
-#define I2C_CL 6 // I2C clock
+//#define I2C_DA 5 // I2C data
+//#define I2C_CL 6 // I2C clock
+
+// Freetronics LeoStick Pins
+// D0 PD2 INT2 RXD1
+// D1 PD3 INT3 TXD1
+// D2 PD1 INT1 SDA
+// D3 PD0 INT0 SCL OC0B
+// D4 A6 PD4 ICP1 ADC8
+// D5 PC6 OC3A ¬OC4A
+// D6 A8 PD7 OC4D ADC10 T0
+// D7 PE6 AIN0
+// D8 A9 PB4 ADC11
+// D9 A10 PB5 OC1A ¬OC4B ADC12 (LED2 Green)
+//#define GREEN_LED 9
+// D10 A11 PB6 OC1B OC4B ADC13 (LED2 Blue)
+//#define BLUE_LED 10
+// D11 PB7 OC0A OC1C RTS (Piezo)
+//#define PIEZO 11
+// D12 PD6 OC4D ADC9
+// D13 PC7 ICP3 CLK0 OC4A (LED2 Red)
+//#define RED_LED 13
+
+// A0 PF7 ADC7 TDI
+// A1 PF6 ADC6 TDO
+// A2 PF5 ADC5 TMS
+// A3 PF4 ADC4 TCK
+// A4 PF1 ADC1
+// A5 PF0 ADC0
+
 
 #endif
 
